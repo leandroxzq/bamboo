3a84c45fe0fbf5f061785009ee8889edeead5f03
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-01-03 12:50:48 -0300

    update footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -5,11 +5,28 @@ function Footer() {
     return (
         <footer className='footer'>
             <div className='footer__wrapper'>
-                <Logo />
-                <div>
+                <div className='footer__logo'>
+                    <Logo />
                     <p>Bamboo</p>
-                    <p>© Bamboo 2024. Todos os direitos reservados.</p>
                 </div>
+                <p className='footer__msg'>
+                    Sua saúde mental é tão importante quanto a física. Reserve
+                    um tempo para você, cuide-se e não hesite em buscar apoio
+                    quando precisar.
+                </p>
+                <div className='footer__contact-card'>
+                    <h3>Entre em Contato</h3>
+                    <a
+                        href='https://mail.google.com/mail/?view=cm&fs=1&to=bamboo@gmail.com'
+                        className='footer__email'
+                        target='_blank'
+                    >
+                        bamboo@gmail.com
+                    </a>
+                </div>
+            </div>
+            <div className='footer__copyright'>
+                <p>© 2025 Bamboo. Todos os direitos reservados.</p>
             </div>
         </footer>
     );
