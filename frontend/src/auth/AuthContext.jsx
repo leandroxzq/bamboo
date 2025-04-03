979f5fb3ce3c36b495ed5136d83120d377ba0cee
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-04-03 20:58:48 -0300

    frontend folder

--- frontend/src/auth/AuthContext.jsx
+++ frontend/src/auth/AuthContext.jsx
@@ -2,7 +2,6 @@ import { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext();
 
-// eslint-disable-next-line react/prop-types
 export const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(localStorage.getItem('token') || null);
     const [role, setRole] = useState(localStorage.getItem('role') || null);
