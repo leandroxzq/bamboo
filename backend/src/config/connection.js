5c85329e5c1d9b6ce3cbbb475d8b5e7c1a126be9
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-04-13 23:56:29 -0300

    correcting connection

--- backend/src/config/connection.js
+++ backend/src/config/connection.js
@@ -1,15 +1,13 @@
-/* eslint-disable no-undef */
-import mysql from "mysql2"
+import mysql from "mysql2/promise"
 import "dotenv/config"
 
-const db = mysql.createConnection({
+const dbPromise = mysql.createPool({
 	host: process.env.BD_HOST,
 	user: process.env.BD_USER,
 	password: process.env.BD_PASSWORD,
 	database: process.env.BD_NAME,
 	timezone: "-03:00",
+	connectionLimit: 10,
 })
-
-const dbPromise = db.promise()
 
 export { dbPromise }
